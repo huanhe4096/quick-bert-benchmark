0ea63c0294eaa4bea6b31221eb03b31ff3c7c767
Huan He <huan.he@yale.edu>
2024-01-11 08:18:32 -0500

    add n_jobs

--- run_tsne_benchmark.py
+++ run_tsne_benchmark.py
@@ -16,11 +16,11 @@ def create_sample(n_samples=10000, dim=384):
 
 
 #%% create tsne
-def run_tsne(X):
+def run_tsne(X, n_jobs=4):
     tsne = TSNE(
         perplexity=30,
         metric="euclidean",
-        n_jobs=8,
+        n_jobs=n_jobs,
         random_state=42,
         verbose=True,
     )
@@ -33,11 +33,11 @@ def run_tsne(X):
     print(f'* ({total_time // 3600:.0f} hours, {(total_time % 3600) // 60:.0f} minutes, {total_time % 60:.2f} seconds)')
 
 
-def main(n_samples=10000, dim=384):
+def main(n_samples=10000, dim=384, n_jobs=4):
     tracemalloc.start()
     # Your memory-intensive code here
     X = create_sample(n_samples, dim)
-    run_tsne(X)
+    run_tsne(X, n_jobs)
     snapshot = tracemalloc.take_snapshot()
     top_stats = snapshot.statistics("lineno")
     for stat in top_stats[:10]:  # Print the top 10 memory-consuming lines
@@ -61,6 +61,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='run tsne benchmark')
     parser.add_argument('--n_samples', type=int, default=10000, help='number of samples')
     parser.add_argument('--dim', type=int, default=384, help='dimension of samples')
+    parser.add_argument('--n_jobs', type=int, default=4, help='number of jobs')
 
     args = parser.parse_args()
     print('* args:', args)
@@ -68,4 +69,5 @@ if __name__ == '__main__':
     main(
         args.n_samples,
         args.dim,
+        args.n_jobs,
     )
