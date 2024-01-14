6b7149d2c0c7d7a92cde7e789a54f98280aec182
Huan He <huan.he@yale.edu>
2024-01-14 15:30:56 -0500

    add texts

--- run_tsne_benchmark.py
+++ run_tsne_benchmark.py
@@ -10,8 +10,12 @@ import tracemalloc
 
 #%% create sample
 def create_sample(n_samples=10000, dim=384):
+    print('* creating sample ...')
+    start_time = time.time()
     X = np.random.rand(n_samples, dim)
+    total_time = time.time() - start_time
     print('* created sample', X.shape)
+    print(f'* Time(seconds): {total_time:.4f}')
     return X
 
 
@@ -31,17 +35,19 @@ def run_tsne(X, n_jobs=4):
     total_time = time.time() - start_time
     print(f'* Time(seconds): {total_time:.4f}')
     print(f'* ({total_time // 3600:.0f} hours, {(total_time % 3600) // 60:.0f} minutes, {total_time % 60:.2f} seconds)')
+    np.save('embds-%s.npy' % (X.shape[0]), embds)
+    print('* saved embds!')
 
 
 def main(n_samples=10000, dim=384, n_jobs=4):
-    tracemalloc.start()
+    # tracemalloc.start()
     # Your memory-intensive code here
     X = create_sample(n_samples, dim)
     run_tsne(X, n_jobs)
-    snapshot = tracemalloc.take_snapshot()
-    top_stats = snapshot.statistics("lineno")
-    for stat in top_stats[:10]:  # Print the top 10 memory-consuming lines
-        print(stat)
+    # snapshot = tracemalloc.take_snapshot()
+    # top_stats = snapshot.statistics("lineno")
+    # for stat in top_stats[:10]:  # Print the top 10 memory-consuming lines
+    #     print(stat)
 
 
 def vis(embds):
@@ -59,9 +65,9 @@ def vis(embds):
 if __name__ == '__main__':
     # add argument parser for n_samples and dim
     parser = argparse.ArgumentParser(description='run tsne benchmark')
-    parser.add_argument('--n_samples', type=int, default=10000, help='number of samples')
-    parser.add_argument('--dim', type=int, default=384, help='dimension of samples')
-    parser.add_argument('--n_jobs', type=int, default=4, help='number of jobs')
+    parser.add_argument('-n', '--n_samples', type=int, default=10000, help='number of samples (10000)')
+    parser.add_argument('-d', '--dim', type=int, default=384, help='dimension of samples (384)')
+    parser.add_argument('-p', '--n_jobs', type=int, default=4, help='number of jobs (4)')
 
     args = parser.parse_args()
     print('* args:', args)
